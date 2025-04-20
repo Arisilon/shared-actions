26b1b33413ec9e0995a0b31a8df32a13ee06aa1e
Jeff <web@pobox.com>
2025-04-20 16:06:36 -0400

    Fix pip usage.

--- .github/actions/install-vjer/action.py
+++ .github/actions/install-vjer/action.py
@@ -11,16 +11,14 @@ def main() -> None:
     parser.add_argument('--vjer-version')
     args = parser.parse_args()
 
-    pypi_test_option = '--index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple' if bool(args.use_pypi_test) else ''  # noqa: E501
     install_version = f'=={args.vjer_version}' if args.vjer_version else ''
-    install_location = '.' if bool(args.vjer_local) else 'vjer'
-    print(['pip', 'install', '--no-cache-dir',
-           pypi_test_option,
-           f'{install_location}{install_version}'])
-    check_call(['pip', 'install', '--no-cache-dir',
-                pypi_test_option,
-                f'{install_location}{install_version}'],
-               shell=True)
+    pip_command = ['pip', 'install', '--no-cache-dir',
+                   '.' if bool(args.vjer_local) else f'vjer{install_version}']
+    if bool(args.use_pypi_test):
+        pip_command += ['--index-url', 'https://test.pypi.org/simple/',
+                        '--extra-index-url', 'https://pypi.org/simple']
+    print(pip_command)
+    check_call(pip_command, shell=True)
 
 
 if __name__ == '__main__':
